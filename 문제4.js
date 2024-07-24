fb7448080250cf32bde9ad54aaa1f752e535f713
hanamstarfield <qnfdmlto@naver.com>
2024-07-24 12:23:16 +0900

    끝!

--- 문제4.js
+++ 문제4.js
@@ -1,22 +1,23 @@
 // 문제 4
 
-function 함수명(a, b, c) {
+function sum(firstnum, Operator, secondnumber) {
     let result = 0;
-    if(b === "+"){
-        return result = a + c;
-    }else if(b === "-"){
-        return result = a - c;
-    }else if(b === "*"){
-        return result = a * c;
-    }else if(b === "/"){
-        return result = a / c;
+    if(Operator === "+"){
+        return result = firstnum + secondnumber;
+    }else if(Operator === "-"){
+        return result = firstnum - secondnumber;
+    }else if(Operator === "*"){
+        return result = firstnum * secondnumber;
+    }else if(Operator === "/"){
+        return result = firstnum / secondnumber;
     }
+}
 
     
     // 코드를 작성해주세요.
 }
 
-console.log(함수명(3, "+", 6)); // 결과값 9
-console.log(함수명(11, "-", 6)); // 결과값 5
-console.log(함수명(6, "*", 3)); // 결과값 18
-console.log(함수명(15, "/", 3)); // 결과값 5
+console.log(sum(3, "+", 6)); // 결과값 9
+console.log(sum(11, "-", 6)); // 결과값 5
+console.log(sum(6, "*", 3)); // 결과값 18
+console.log(sum(15, "/", 3)); // 결과값 5
