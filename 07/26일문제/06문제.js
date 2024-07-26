bb24ef410fcf3135716270526e2dee99e2599f62
hanamstarfield <qnfdmlto@naver.com>
2024-07-26 12:59:13 +0900

    crear수정

--- 07/26일문제/06문제.js
+++ 07/26일문제/06문제.js
@@ -2,17 +2,16 @@
 
 1.
 const students = [
-  { name: "이재상", scores: [80, 90] },
-  { name: "김준현", scores: [90, 95] }
-  { name: "정윤오", scores: [75, 70] },
- 
+    { name: "이재상", scores: [80, 90] },
+    { name: "김준현", scores: [90, 95] },
+    { name: "정윤오", scores: [75, 70] },
+
 ];
 
 // 여기에 코드를 작성해주세요
 // 예상 출력: ["이재상", "김준현"]
-const names = students.filter(student =>
-{
-    const aver = (students.scores[0] + students.scores[1]) / 2;
+const names = students.filter(student => {
+    const aver = (student.scores[0] + student.scores[1]) / 2;
     return aver >= 80;
 }).map(student => student.name);
 console.log(names);
@@ -24,3 +23,4 @@ const numbers = [5, 10, 15, 20, 25];
 // 요소에 2를 곱하고, 30 이하인 것들만 반환받아 출력해주세요
 // 출력값 : [10, 20, 30]
 let newnumber = numbers.map(number => number * 2).filter(number => number <= 30);
+console.log(newnumber);
